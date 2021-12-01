da9dd632a423d48cfd65cd6901b54a75d9a8ae8b
Matthew Sherborne <msherborne@gmail.com>
2021-12-02 02:23:44 +1000

    More comment improvements

--- day1/src/part2.rs
+++ day1/src/part2.rs
@@ -2,14 +2,14 @@ use itertools::Itertools;
 
 use crate::shared::{input_to_numbers, IncreaseCounter};
 
-/// Given a list of numbers, add up a 3 length sliding window, then return how many sliding windows are larger than the previous sliding window
+/// Given a list of numbers, add up a 3 length sliding window, then return how
+/// many sliding windows are larger than the previous sliding window
 pub fn part2(input: &str) -> usize {
     input_to_numbers(input)
         .tuple_windows()
         .inspect(|window| log::debug!("Window: {:?}", window))
         .map(|(a, b, c)| a + b + c)
         .inspect(|sum| log::debug!("Sum: {}", sum))
-        // Count how many are an increase
         .count_increases()
 }
 
